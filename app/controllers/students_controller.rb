eefab346676d74c4b31f917f1a81b791bc4cf9b3
QunFan <allenfan1012@gmail.com>
2023-02-06 10:46:06 +1100

    commit tree 4f8bcec5cc04f9a003f9d42244c388b76b354a3a

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index
+        students=Student.all 
+        render json: students
+    end
+
+    def grades
+        grades=Student.all.order(grade: :desc)
+        render json: grades
+    end
+
+    def highest_grade
+        grade=Student.all.order(grade: :desc).first
+        render json: grade 
+    end
+
+end
